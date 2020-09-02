9f5d072227ea5d24c530418bf4914ba4ad3032cf
Ammar Faizi <ammarfaizi2@gmail.com>
2020-09-02 09:40:11 +0700

    Add more default workers

--- start.sh
+++ start.sh
@@ -5,26 +5,27 @@
 /bin/mkdir -vp /run/sshd;
 
 # Run SSHD.
-rm -vf /etc/ssh/sshd_config && \
-cp -vf /opt/works/etc/ssh/sshd_config /etc/ssh/sshd_config;
+/bin/rm -vf /etc/ssh/sshd_config && \
+/bin/cp -vf /opt/works/etc/ssh/sshd_config /etc/ssh/sshd_config;
 exec /usr/sbin/sshd -D &
 
 # Run PHP.
-cp -vf /opt/works/etc/php/7.4/fpm/php.ini /etc/php/7.4/fpm/php.ini;
-cp -vf /opt/works/etc/php/7.4/fpm/php-fpm.conf /etc/php/7.4/fpm/php-fpm.conf;
-cp -vf /opt/works/etc/php/7.4/fpm/pool.d/www.conf /etc/php/7.4/fpm/pool.d/www.conf;
+/bin/mkdir -vp /var/log/php/
+/bin/cp -vf /opt/works/etc/php/7.4/fpm/php.ini /etc/php/7.4/fpm/php.ini;
+/bin/cp -vf /opt/works/etc/php/7.4/fpm/php-fpm.conf /etc/php/7.4/fpm/php-fpm.conf;
+/bin/cp -vf /opt/works/etc/php/7.4/fpm/pool.d/www.conf /etc/php/7.4/fpm/pool.d/www.conf;
 /usr/sbin/php-fpm7.4 --daemonize --fpm-config /etc/php/7.4/fpm/php-fpm.conf &
 
-isolate --version || (
-git clone https://github.com/ioi/isolate /root/isolate &&
+/usr/local/bin/isolate --version || (
+/usr/bin/git clone https://github.com/ioi/isolate /root/isolate &&
 cd /root/isolate &&
-make;
-sed -i '/num_boxes = 1000/c\num_boxes = 69000' /root/isolate/default.cf;
-make install;
+/usr/bin/make;
+/usr/bin/sed -i '/num_boxes = 1000/c\num_boxes = 69000' /root/isolate/default.cf;
+/usr/bin/make install;
 )
 
-sed -i 's/rights="none" pattern="PDF"/rights="read|write" pattern="PDF"/g' /etc/ImageMagick-6/policy.xml;
+/usr/bin/sed -i 's/rights="none" pattern="PDF"/rights="read|write" pattern="PDF"/g' /etc/ImageMagick-6/policy.xml;
 
-apt-fast install -y texlive-full;
+/usr/local/sbin/apt-fast install -y texlive-full;
 
 wait
