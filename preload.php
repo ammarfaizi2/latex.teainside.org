569cdcb8ad745e8bca84411244163a2755e5a66f
Ammar Faizi <ammarfaizi2@gmail.com>
2020-09-02 09:21:50 +0700

    Add preload script

--- preload.php
+++ preload.php
@@ -2,7 +2,7 @@
 
 define("OPCACHE_PRELOAD", true);
 
-require_once __DIR__."/../config.php";
-require_once __DIR__."/../src/autoload.php";
+require_once __DIR__."/config.php";
+require_once __DIR__."/src/autoload.php";
 
 var_dump(class_exists("TeaLatex\\TeaLatex"));
